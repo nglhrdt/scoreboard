47e5dd0fa531b1d8c7cf2f316074dbd3bb1f4c8a
Simon Engelhardt <smn@nglhrdt.de>
2025-06-19 18:40:46 +0000

    use http instead of tcp for mqtt connection

--- client/src/MqttBoard.tsx
+++ client/src/MqttBoard.tsx
@@ -1,44 +1,45 @@
-import mqtt, { MqttClient } from "mqtt";
-import { type FC, useEffect, useState } from "react";
+import mqtt, { MqttClient } from 'mqtt';
+import { type FC, useEffect, useState } from 'react';
 
 const MqttBoard: FC = () => {
   const [client, setClient] = useState<MqttClient | null>(null);
   const [messages, setMessages] = useState<string[]>([]);
-  const [connectionStatus, setConnectionStatus] = useState("Disconnected");
+  const [connectionStatus, setConnectionStatus] = useState('Disconnected');
+  const topic = 'scoreboard/messages'; // Define the topic
 
   useEffect(() => {
     const mqttClient = mqtt.connect({
-      port: 8883,
-      host: "mqttws.devilsoft.de",
-      protocol: "mqtts",
+      port: 8084,
+      host: 'mqttws.devilsoft.de',
+      protocol: 'wss',
     });
     setClient(mqttClient);
 
-    mqttClient.on("connect", () => {
-      setConnectionStatus("Connected");
+    mqttClient.on('connect', () => {
+      setConnectionStatus('Connected');
       mqttClient.subscribe(topic, (err) => {
         if (err) {
-          console.error("Subscribe error:", err);
+          console.error('Subscribe error:', err);
         }
       });
     });
 
-    mqttClient.on("message", (topic, payload) => {
+    mqttClient.on('message', (topic, payload) => {
       const message = payload.toString();
       setMessages((prevMessages) => [...prevMessages, message]);
     });
 
-    mqttClient.on("error", (err) => {
-      console.error("Connection error: ", err);
+    mqttClient.on('error', (err) => {
+      console.error('Connection error: ', err);
       mqttClient.end();
     });
 
-    mqttClient.on("reconnect", () => {
-      setConnectionStatus("Reconnecting");
+    mqttClient.on('reconnect', () => {
+      setConnectionStatus('Reconnecting');
     });
 
-    mqttClient.on("close", () => {
-      setConnectionStatus("Disconnected");
+    mqttClient.on('close', () => {
+      setConnectionStatus('Disconnected');
     });
 
     return () => {
